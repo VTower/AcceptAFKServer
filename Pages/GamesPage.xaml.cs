a9fcb24d36a1c43410cb9d6250909e183ee38d8b
Victor <signalpack326@gmail.com>
2024-11-21 23:24:38 -0300

    Adicionado navegação de Homepage e Logo dos games

---
+++ Pages/GamesPage.xaml.cs
@@ -0,0 +1,10 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace AcceptAFKServer.Pages;
+public sealed partial class GamesPage : Page
+{
+    public GamesPage()
+    {
+        InitializeComponent();
+    }
+}
--- Pages/HomePage.xaml.cs
+++ Pages/HomePage.xaml.cs
@@ -21,7 +21,24 @@ public sealed partial class HomePage : Page
 
     private void NavigateBarButton(object sender, RoutedEventArgs e)
     {
-        ContentFrame.Navigate(typeof(ConfigurationPage));
+        Console.WriteLine("Botão clicado!");
+        Button button = (Button)sender;
+
+        switch (button.Name)
+        {
+            case "Lupa":
+                if (ContentFrame.CurrentSourcePageType != typeof(GamesPage))
+                    ContentFrame.Navigate(typeof(GamesPage));
+                break;
+            case "Config":
+                if (ContentFrame.CurrentSourcePageType != typeof(ConfigurationPage))
+                    ContentFrame.Navigate(typeof(ConfigurationPage));
+                break;
+            case "Wifi":
+                if (ContentFrame.CurrentSourcePageType != typeof(WifiPage))
+                    ContentFrame.Navigate(typeof(WifiPage));
+                break;
+        }
     }
 
     // TODO: Mudar nome e ver se vou manter 
@@ -38,27 +55,28 @@ public sealed partial class HomePage : Page
         List<UIElement> icons =
         [
             //Lupa
-            CreateButtonWithIcon("\uE721"),
+            CreateButtonWithIcon("\uE721", "Lupa"),
             //Config
-            CreateButtonWithIcon("\uE713"),
+            CreateButtonWithIcon("\uE713", "Config"),
             //Wifi
-            CreateButtonWithIcon("\uE701")
+            CreateButtonWithIcon("\uE701", "Wifi")
         ];
 
         MyFlipView.ItemsSource = icons;
     }
 
-    private Button CreateButtonWithIcon(string glyph)
+    private Button CreateButtonWithIcon(string glyph, string name = null)
     {
         Button button = new()
         {
+            Name = name,
             Content = new FontIcon
             {
                 Glyph = glyph,
-                FontSize = 100
+                FontSize = 60
             },
-            HorizontalAlignment = HorizontalAlignment.Center,
-            VerticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Stretch,
+            VerticalAlignment = VerticalAlignment.Stretch,
         };
 
         button.Click += NavigateBarButton;
